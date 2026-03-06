438d71afbc2251fe8815bd9cf1b1251cfc689844
vedika0619 <ghosalkarvedika99@gmail.com>
2026-03-06 07:56:37 -0800

    added login page

---
+++ script.js
@@ -0,0 +1,12 @@
+function openLogin(){
+document.getElementById("loginPopup").style.display="flex";
+}
+
+function openSignup(){
+document.getElementById("signupPopup").style.display="flex";
+}
+
+function closeForms(){
+document.getElementById("loginPopup").style.display="none";
+document.getElementById("signupPopup").style.display="none";
+}
